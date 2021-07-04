d3452dca3cae6c7150ba46f6620aa12c6b7b7c1a
ZdraviiSmisl <55047650+ZdraviiSmisl@users.noreply.github.com>
2021-07-04 19:05:56 +0300

    модалки,js  главная страница

---
+++ address.js
@@ -0,0 +1,23 @@
+const mapLink=document.querySelector(".company-info__link");
+
+const mapPopup=document.querySelector('.modal-map');
+const mapClouse=document.querySelector('.modal-map__modal-clouse');
+
+mapLink.addEventListener('click',function (evt)=>{
+  evt.preventDefault();
+  mapPopup.classList.add('modal-show');
+});
+
+mapClouse.addEventListener('click',function (evt)=>{
+  evt.preventDefault();
+  mapPopup.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown',function (evt)=>{
+  if(evt.keyCode===27){
+    if(mapPopup.classList.contains('modal-show')){
+      evt.preventDefault();
+      mapPopup.classList.remove('modal-show');
+    }
+  }
+});
---
+++ feedbackPopup.js
@@ -0,0 +1,23 @@
+const feedbackLink=document.body.querySelector('.company-info__btn');
+const popupForm=document.body.querySelector('.modal-feedback');
+const popupClouse=document.body.querySelector('.form-feedback__modal-close ');
+
+
+feedbackLink.addEventListener('click',function (evt)=>{
+  evt.preventDefault();
+  popupForm.classList.add('modal-show');
+});
+
+popupClouse.addEventListener('click',function (evt)=>{
+  evt.preventDefault();
+  popupForm.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown',function (evt)=>{
+  if(evt.keyCode===27){
+    if(popupForm.classList.contains('modal-show')){
+      evt.preventDefault();
+      popupForm.classList.remove('modal-show');
+    }
+  }
+});
